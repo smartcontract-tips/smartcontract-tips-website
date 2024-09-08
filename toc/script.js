4cafc26179e79151ed539c50eb04f4f5589a7bf6
davide <davide@mac-mini.local>
2024-09-08 10:17:23 +0200

    fixed the toc resize

--- toc/script.js
+++ toc/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const toc = document.getElementById('toc');
 
   // Funzione per controllare la larghezza della finestra e nascondere il div "toc" se la larghezza è inferiore a 1350px
+  /** 
   const handleResize = () => {
     if (window.innerWidth < 1350) {
       toc.style.display = 'none';
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Aggiungi un listener per l'evento di ridimensionamento della finestra
   window.addEventListener('resize', handleResize);
-
+*/
 
   // Funzione per controllare se il TOC è in sovrapposizione con altri elementi
   const toggleTOCVisibility = (entries) => {
